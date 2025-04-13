c09495c893878b214ace19b42dce3c45d07d7696
Matthew Pham <79127545+MatthewPham101@users.noreply.github.com>
2025-04-13 14:25:12 -0700

    added models

---
+++ petportal_2/PetWebpage/models/pet.js
@@ -0,0 +1,70 @@
+// models/pet.js
+const { DataTypes } = require('sequelize');
+const sequelize = require('../database');
+
+const Pet = sequelize.define('Pet', {
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  type: {
+    type: DataTypes.ENUM('dog', 'cat', 'bird', 'rabbit', 'hamster', 'fish', 'reptile', 'other'),
+    allowNull: false
+  },
+  breed: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  age: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  ageUnit: {
+    type: DataTypes.ENUM('days', 'weeks', 'months', 'years'),
+    allowNull: false,
+    defaultValue: 'months'
+  },
+  gender: {
+    type: DataTypes.ENUM('male', 'female', 'unknown'),
+    allowNull: false
+  },
+  size: {
+    type: DataTypes.ENUM('small', 'medium', 'large', 'extra-large'),
+    allowNull: false
+  },
+  color: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  description: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  isVaccinated: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false
+  },
+  status: {
+    type: DataTypes.ENUM('available', 'pending', 'adopted', 'not_available'),
+    defaultValue: 'available'
+  },
+  specialNeeds: {
+    type: DataTypes.TEXT,
+    allowNull: true
+  },
+}, {
+  tableName: 'pets',
+  indexes: [
+    {
+      fields: ['type']
+    },
+    {
+      fields: ['status']
+    }
+  ]
+});
+
+module.exports = Pet;
